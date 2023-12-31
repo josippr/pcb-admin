2becafc61075922e3c9da7f9a87ca1ff6556544c
unknown <josippr@gmail.com>
2023-12-31 15:31:06 +0100

    added react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import React from "react"
 import './styles/global.css';
 
+// components
 import Header from "./components/Header/Header";
+
+// pages
+import Users from "./pages/Users/Users";
+
 
 function App() {
   return (
